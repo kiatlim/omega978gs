e5ddeeb1dda853573625d180a65b296f020746b9
Kiat Lim <kiat@pixelonion.com>
2012-06-08 16:46:30 +0800

    latest update

--- template.php
+++ template.php
@@ -22,4 +22,53 @@ function pixelonionkit_form_alter(&$form, &$form_state, $form_id) {
   }
 }
 
-?>
+// Added Span Class to Breadcrumb so we can customize it if needed
+function pixelonionkit_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are not here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' <span>&raquo;</span> ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
+
+/**
+ * Implements hook_process_region().
+ */
+function pixelonionkit_process_region(&$vars) {
+  if (in_array($vars['elements']['#region'], array('content', 'menu', 'branding'))) {
+    $theme = alpha_get_theme();
+
+    switch ($vars['elements']['#region']) {
+      case 'content':
+        $vars['title_prefix'] = $theme->page['title_prefix'];
+        $vars['title'] = $theme->page['title'];
+        $vars['title_suffix'] = $theme->page['title_suffix'];
+        $vars['tabs'] = $theme->page['tabs'];
+        $vars['action_links'] = $theme->page['action_links'];
+        $vars['title_hidden'] = $theme->page['title_hidden'];
+        $vars['feed_icons'] = $theme->page['feed_icons'];
+        break;
+
+      case 'menu':
+        $vars['main_menu'] = $theme->page['main_menu'];
+        $vars['secondary_menu'] = $theme->page['secondary_menu'];
+        break;
+
+      case 'branding': //Changes H1 Title from "Home" to Site Name
+        $vars['site_name'] = $theme->page['site_name'];
+        $vars['linked_site_name'] = l($vars['site_name'], '<front>', array('attributes' => array('rel' => 'home', 'title' => $theme->page['site_name']), 'html' => TRUE));
+        $vars['site_slogan'] = $theme->page['site_slogan'];
+        $vars['site_name_hidden'] = $theme->page['site_name_hidden'];
+        $vars['site_slogan_hidden'] = $theme->page['site_slogan_hidden'];
+        $vars['logo'] = $theme->page['logo'];
+        $vars['logo_img'] = $vars['logo'] ? '<img src="' . $vars['logo'] . '" alt="' . check_plain($vars['site_name']) . '" id="logo" />' : '';
+        $vars['linked_logo_img'] = $vars['logo'] ? l($vars['logo_img'], '<front>', array('attributes' => array('rel' => 'home', 'title' => check_plain($vars['site_name'])), 'html' => TRUE)) : '';
+        break;
+    }
+  }
+}
